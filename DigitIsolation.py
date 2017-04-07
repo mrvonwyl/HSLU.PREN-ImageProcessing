19774da79c267949ad2d3602771a81d5e9a878d5
mrvonwyl <marcel.vonwyl@outlook.com>
2017-04-07 11:01:40 +0200

    crop serifes, minor changes

--- DigitIsolation.py
+++ DigitIsolation.py
@@ -1,115 +1,158 @@
 import numpy as np
 import cv2
+import copy
+import math
 
 
-def mask_color(image, boundaries):
-    lower = np.array(boundaries[0][0], dtype="uint8")
-    upper = np.array(boundaries[0][1], dtype="uint8")
-    return cv2.inRange(image, lower, upper)
+class DigitIsolation:
+    @staticmethod
+    def mask_color(image, boundaries):
+        lower = np.array(boundaries[0][0], dtype="uint8")
+        upper = np.array(boundaries[0][1], dtype="uint8")
+        return cv2.inRange(image, lower, upper)
 
+    @staticmethod
+    def order_points(pts):
+        # initialzie a list of coordinates that will be ordered
+        # such that the first entry in the list is the top-left,
+        # the second entry is the top-right, the third is the
+        # bottom-right, and the fourth is the bottom-left
+        rect = np.zeros((4, 2), dtype = "float32")
 
-def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype = "float32")
+        # the top-left point will have the smallest sum, whereas
+        # the bottom-right point will have the largest sum
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
 
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
-    s = pts.sum(axis = 1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
+        # now, compute the difference between the points, the
+        # top-right point will have the smallest difference,
+        # whereas the bottom-left will have the largest difference
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
 
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
-    diff = np.diff(pts, axis = 1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
+        # return the ordered coordinates
+        return rect
 
-    # return the ordered coordinates
-    return rect
+    @staticmethod
+    def four_point_transform(image, pts):
+        # obtain a consistent order of the points and unpack them
+        # individually
+        rect = DigitIsolation.order_points(pts)
+        (tl, tr, br, bl) = rect
 
+        # compute the width of the new image, which will be the
+        # maximum distance between bottom-right and bottom-left
+        # x-coordiates or the top-right and top-left x-coordinates
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
 
-def four_point_transform(image, pts):
-    # obtain a consistent order of the points and unpack them
-    # individually
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
+        # compute the height of the new image, which will be the
+        # maximum distance between the top-right and bottom-right
+        # y-coordinates or the top-left and bottom-left y-coordinates
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
 
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
+        # now that we have the dimensions of the new image, construct
+        # the set of destination points to obtain a "birds eye view",
+        # (i.e. top-down view) of the image, again specifying points
+        # in the top-left, top-right, bottom-right, and bottom-left
+        # order
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype="float32")
 
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
+        # compute the perspective transform matrix and then apply it
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype = "float32")
+        # return the warped image
+        return warped
 
-    # compute the perspective transform matrix and then apply it
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    @staticmethod
+    def isolate_roman_digit(img):
+        black_boundaries = [([0, 0, 0], [50, 50, 50])]
+        red_boundaries = [([0, 15, 100], [70, 70, 210])]
 
-    # return the warped image
-    return warped
+        black = DigitIsolation.mask_color(img, black_boundaries)
+        red = DigitIsolation.mask_color(img, red_boundaries)
 
+        cv2.imshow('b', black)
 
-def isolate_roman_digit(img):
-    black_boundaries= [([0, 0, 0], [50, 50, 50])]
-    red_boundaries = [([0, 15, 100], [70, 70, 210])]
+        kernel = np.ones((5, 5), np.uint8)
+        red = cv2.morphologyEx(red, cv2.MORPH_CLOSE, kernel)
+        red = cv2.morphologyEx(red, cv2.MORPH_OPEN, kernel)
 
-    black = mask_color(img, black_boundaries)
-    red = mask_color(img, red_boundaries)
+        cv2.imshow("redMask", red)
+        im2, contours, hierarchy = cv2.findContours(red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
 
-    kernel = np.ones((5, 5), np.uint8)
-    red = cv2.morphologyEx(red, cv2.MORPH_CLOSE, kernel)
-    red = cv2.morphologyEx(red, cv2.MORPH_OPEN, kernel)
+        i = 0
+        rect = np.zeros((4, 2), dtype="float32")
 
-    cv2.imshow("redMask", red)
+        for cnt in contours:
+            epsilon = 0.01 * cv2.arcLength(cnt, True)
 
-    im2, contours, hierarchy = cv2.findContours(red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
+            # print(epsilon)
 
-    i = 0
-    rect = np.zeros((4, 2), dtype = "float32")
+            if epsilon >= 2:
+                approx = cv2.approxPolyDP(cnt, epsilon, True)
+                pts = np.array([tuple(approx[0][0]), tuple(approx[1][0]), tuple(approx[2][0]), tuple(approx[3][0])], dtype="float32")
+                pts = DigitIsolation.order_points(pts)
 
-    for cnt in contours:
-        epsilon = 0.01 * cv2.arcLength(cnt, True)
+                if i == 0:
+                    rect[0] = pts[1]
+                    rect[1] = pts[2]
 
-        print(epsilon)
+                    # cv2.rectangle(img, (10, 10), (30, 30), (0, 255, 0), 2)
+                elif i == 1:
+                    rect[2] = pts[0]
+                    rect[3] = pts[3]
 
-        if epsilon >= 2:
-            approx = cv2.approxPolyDP(cnt, epsilon, True)
-            pts = np.array([tuple(approx[0][0]), tuple(approx[1][0]), tuple(approx[2][0]), tuple(approx[3][0])], dtype = "float32")
-            pts = order_points(pts)
+                i += 1
 
-            if i == 0:
-                rect[0] = pts[1]
-                rect[1] = pts[2]
+        warped = DigitIsolation.four_point_transform(black, rect)
+        resized = cv2.resize(warped, (800, 450), interpolation=cv2.INTER_CUBIC)
 
-                cv2.rectangle(img, (10, 10), (30, 30), (0, 255, 0), 2)
-            elif i == 1:
-                rect[2] = pts[0]
-                rect[3] = pts[3]
+        resized = DigitIsolation.digit_bounding_box(resized)
 
-            i += 1
+        return resized
 
-    warped = four_point_transform(black, rect)
-    resized = cv2.resize(warped, (800, 450), interpolation=cv2.INTER_CUBIC)
-    return resized
+    @staticmethod
+    def digit_bounding_box(img):
+
+        cv2.imshow('asd', img)
+
+        margin = 0.2
+
+        black = copy.copy(img)
+
+        kernel = np.ones((5, 5), np.uint8)
+        black = cv2.morphologyEx(black, cv2.MORPH_CLOSE, kernel)
+        black = cv2.morphologyEx(black, cv2.MORPH_OPEN, kernel)
+
+        im2, contours, hierarchy = cv2.findContours(black, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
+
+        offsetX = 0
+        width = 0
+        offsetY = 0
+        height = 0
+
+        for contour in contours:
+            [x, y, w, h] = cv2.boundingRect(contour)
+            if w * h > width * height and w > 10 and h > 10:
+                width = w
+                height = h
+                offsetX = x
+                offsetY = y
+
+        y = math.floor(y + (h * margin))
+        h = math.floor(h * (1 - 2 * margin))
+
+        img = img[y:y+h, x:x+w]
+
+        return img
--- MainZifferErkennung.py
+++ MainZifferErkennung.py
@@ -2,23 +2,23 @@ import time
 
 import cv2
 
-from DigitIsolation import isolate_roman_digit
+from DigitIsolation import DigitIsolation
 
 img = cv2.imread('images/4.png')
 
-cap = cv2.VideoCapture('images/v1.webm')
+cap = cv2.VideoCapture('images/v2.webm')
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # img = frame
     cv2.imshow("orig", img)
-    resized = isolate_roman_digit(img)
+    resized = DigitIsolation.isolate_roman_digit(img)
     cv2.imshow('resized', resized)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    time.sleep(.100)
+    time.sleep(.500)
 
 # When everything done, release the capture
 cap.release()
