4c851e4872548d12bc6320adef93750a637c910f
Alexander Morard <alex_morard2@hotmail.com>
2017-04-27 10:56:50 +0200

    Refactoring Ampelerkennung

--- MainAmpelerkennung.py
+++ MainAmpelerkennung.py
@@ -1,9 +1,6 @@
-from collections import deque
-import time
 import numpy as np
-import imutils
 import cv2
-from TrafficLightDetection import color_detection
+from TrafficLightDetection import *
 
 # start the video capture
 camera = cv2.VideoCapture(0)
@@ -16,50 +13,24 @@ traffic_light = False
 while True:
 	# grab the current frame
 	_, frame = camera.read()
+
+        #set the color for color detection
+	color = color_vallue('red')
 	
-	# resize the frame, blur it, and convert it to the HSV color space
-	frame = imutils.resize(frame, width = 600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	# prepare the frame for color detection
+	frame = frame_prep()
 	
 	# create a mask by checking for red and green traffic lights
-	mask = color_detection(traffic_light, hsv)
+	mask = color_detection(traffic_light, frame, color)
 
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
+	contour = contour_detection(mask)
 	
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key = cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-			#change the state of the traffic light
-			if (traffic_light):
-				# when the traffic light is TRUE then set it to FALSE
-				traffic_light = False
-			
-			else:
-				# when the traffic light is FALSE then set it to TRUE
-				traffic_light = True
-			
-			print (traffic_light)
-			time.sleep(.5)
+	# give the go-signal when a matching contour was found
+	traffic_light = go_time(contour)
 	
-	# display the original frame the mask amd the result of ovelaying
+	# display the original frame the mask amd the result of overlaying
 	# the mask with the original frame		
 	cv2.imshow('frame',frame)
 	cv2.imshow('mask', mask)
