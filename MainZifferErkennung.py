d49732aecafde4220adb761fdb568ef9c6acf9f2
mrvonwyl <marcel.vonwyl@outlook.com>
2017-04-28 12:53:11 +0200

    cap Settings

--- MainZifferErkennung.py
+++ MainZifferErkennung.py
@@ -5,21 +5,38 @@ import cv2
 from DigitIsolation import DigitIsolation as di
 from DigitRecognition import DigitRecognition as dr
 
-img = cv2.imread('images/Nr4.png')
+img = cv2.imread('images/33.jpg')
 
-cap = cv2.VideoCapture('images/v2.webm')
+cap = cv2.VideoCapture(0)
+
+# cap.set(3, 1280)
+# cap.set(4, 720)
+# brightness
+cap.set(11, 2000)
+# exposure
+# cap.set(16, 0.1)
+# contrast
+cap.set(12, 10)
+# gain
+cap.set(15, -10)
+# FPS
+# cap.set(6, 0.1)
+
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    # img = frame
+    img = frame
     cv2.imshow("orig", img)
-    resized = di.isolate_roman_digit(img)
+    #resized = di.isolate_roman_digit(img)
+    #cv2.imshow('resized', resized)
+    #number = dr.recognize_digit(resized)
+    #print(number);
 
-    number = dr.recognize_digit(resized)
+    grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    print(number);
+    cv2.imshow("grey", grey)
 
-    cv2.imshow('resized', resized)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
