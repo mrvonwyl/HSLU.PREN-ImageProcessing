d2d96f96569e4710cd1e1a2845f166121393c630
Alexander Morard <alex_morard2@hotmail.com>
2017-05-04 10:53:46 +0200

    Update Ampelerkennung

--- MainAmpelerkennung.py
+++ MainAmpelerkennung.py
@@ -1,43 +1,49 @@
 import numpy as np
 import cv2
+import time
 from TrafficLightDetection import *
+
+print ('Start Traffic Light Detection')
 
 # start the video capture
 camera = cv2.VideoCapture(0)
 
-# initialize boolean for the traffic light
-# initial state red = FALSE and green = TRUE
+# initialize booleans for traffic light detection
+# loop: initial state TRUE to run the loop and FALSE to stop the loop
+# traffic_light: initial state red = FALSE and green = TRUE
+loop = True
 traffic_light = False
 
 # looping while video capture is active
-while True:
-	# grab the current frame
-	_, frame = camera.read()
+while loop:
+        # grab the current frame
+        _, frame = camera.read()
+        cv2.imshow('frame', frame)
+        
+        # prepare the frame for color detection
+        frame = frame_prep(frame)
 
-        #set the color for color detection
-	color = color_vallue('red')
-	
-	# prepare the frame for color detection
-	frame = frame_prep()
-	
-	# create a mask by checking for red and green traffic lights
-	mask = color_detection(traffic_light, frame, color)
+        # set the color for color detection and
+        # create a mask by checking for red and green traffic lights
+        mask = color_detection(frame, 'red')
+        cv2.imshow('mask', mask)
 
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	contour = contour_detection(mask)
-	
-	# give the go-signal when a matching contour was found
-	traffic_light = go_time(contour)
-	
-	# display the original frame the mask amd the result of overlaying
-	# the mask with the original frame		
-	cv2.imshow('frame',frame)
-	cv2.imshow('mask', mask)
-	
-	# stop the script by pressing 'q'
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        contour = contour_detection(mask)
+        
+        # give the go-signal when a matching contour was found
+        traffic_light = go_time(contour)
+        
+        # display the original frame the mask amd the result of
+        # overlaying the mask with the original frame           
+        print (traffic_light)
+        time.sleep(.5) 
+        
+        # stop the loop if it is go time
+        if (traffic_light == True):
+                loop = False
 
+print ('Go Time')
 cv2.release()
 cv2.destroyAllWindows()
--- TrafficLightDetection.py
+++ TrafficLightDetection.py
@@ -3,88 +3,68 @@ import numpy as np
 import imutils
 import cv2
 
-class TrafficLightDetection
-        #returns the numpy array for the color red in the hsv color space
-        def color_value(color):
-                if (color == 'red'):
-                        color = np.array([170, 100, 100],
-                                         [180, 255, 255],
-                                         [  0, 100, 100],
-                                         [ 10, 255, 255])
-                
-                elif (color == 'green'):
-                        color = np.array([170, 100, 100],
-                                         [180, 255, 255])
-                return color
+# returns a resized, blurred and HSV color space converted frame
+def frame_prep(frame):
+        frame = imutils.resize(frame, width = 600)
+        frame = cv2.GaussianBlur(frame, (11, 11), 0)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        return frame
 
-        # returns a resized, blurred and HSV color space converted frame 
-        def frame_prep():
-                frame = imutils.resize(frame, width = 600)
-                frame = cv2.GaussianBlur(frame, (11, 11), 0)
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-                return frame
+# detects the given color and creates a bit mask for said color
+def color_detection(frame, color):
 
-        # detects the given color and creates a bit mask for said color               
-        def color_detection(traffic_light, frame, color):
-        
-        	# checking the state of the traffic light
-        	if not (traffic_light):
-        	
-        		# construct a mask for the given color,
-        		# then perform a series of dilations and erosions to
-        		# remove any small blobs left in the mask
-                	#mask_top = cv2.inRange(frame, lower_red_top, upper_red_top)
-                	mask_top = cv2.inRange(frame, color[0, 0:2], color[1, 0:2])
-        		mask_top = cv2.erode(mask_top, None, iterations = 2)
-                	mask_top = cv2.dilate(mask_top, None, iterations = 2)
-        	
-                	# construct a mask for the bottom bound of the HSV color "red",
-                        # then perform a series of dilations and erosions to
-                	# remove any small blobs left in the mask
-        		#mask_bottom = cv2.inRange(hsv, lower_red_bottom, upper_red_bottom)
-        		mask_bottom = cv2.inRange(frame, color[2, 0:2], color[3, 0:2])
-        		mask_bottom = cv2.erode(mask_bottom, None, iterations = 2)
-        		mask_bottom = cv2.dilate(mask_bottom, None, iterations = 2)     
-        
-        		# combine the top and bottom masks to create the complete mask
-        		mask = cv2.add(mask_top, mask_bottom)
-        		
-                return mask
+        if (color == 'red'):
+                top_min = np.array([170, 100, 100])
+                top_max = np.array([170, 100, 100])
+                bottom_min = np.array([170, 100, 100])
+                bottom_max = np.array([170, 100, 100])
 
-        # returns the found contours in the given mask
-        def contour_detection(mask):
-                contour = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-                return contour
+        elif (color == 'green'):
+                top = np.array([170, 100, 100])
+                bottom = np.array([170, 100, 100])
 
-        # returns the constraints for the contours
-        def contour_constraints():
-                return constraints
+        # construct a mask for the given color,
+        # then perform a series of dilations and erosions to
+        # remove any small blobs left in the mask
+        #mask_top = cv2.inRange(frame, lower_red_top, upper_red_top)
+        mask_top = cv2.inRange(frame, top_min, top_max)
+        mask_top = cv2.erode(mask_top, None, iterations = 2)
+        mask_top = cv2.dilate(mask_top, None, iterations = 2)
 
-        # returns true or false wether the contours match the given constraints
-        def go_time(contour);
-                if len(contour) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and centroid
-		c = max(contour, key = cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        # construct a mask for the bottom bound of the HSV color "red",
+        # then perform a series of dilations and erosions to
+        # remove any small blobs left in the mask
+        #mask_bottom = cv2.inRange(hsv, lower_red_bottom, upper_red_bottom)
+        mask_bottom = cv2.inRange(frame, bottom_min, bottom_max)
+        mask_bottom = cv2.erode(mask_bottom, None, iterations = 2)
+        mask_bottom = cv2.dilate(mask_bottom, None, iterations = 2)
 
-			#change the state of the traffic light
-			if (traffic_light):
-				# when the traffic light is TRUE then set it to FALSE
-				return False
-			
-			else:
-				# when the traffic light is FALSE then set it to TRUE
-				return True
-			
-			print (traffic_light)
-			time.sleep(.5) 
+        # combine the top and bottom masks to create the complete mask
+        mask = cv2.add(mask_top, mask_bottom)
+        return mask
+
+# returns the found contours in the given mask
+def contour_detection(mask):
+        contour = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        return contour
+
+# returns TRUE or FALSE wether the contours match the given constraints
+def go_time(contour):
+
+        # only proceed if at least one contour was found
+        if len(contour) > 0:
+
+                # find the largest contour in the mask, then use
+                # it to compute the minimum enclosing circle and centroid
+                c = max(contour, key = cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+                # only proceed if the radius meets a minimum and maximum size
+                if (radius > 10 and radius < 15):
+                        return True
+                else:
+                        return False
+        else:
+                return False
