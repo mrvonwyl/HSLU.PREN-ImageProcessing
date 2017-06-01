9f283b414c3b5625dba9709c46169ad9374bcc50
Alexander Morard <alex_morard2@hotmail.com>
2017-06-01 09:49:10 +0200

    update Ampelerkennung

--- AmpelMain.py
+++ AmpelMain.py
@@ -12,7 +12,12 @@ ser = Serial()
 camera = cv2.VideoCapture(0)
 
 # Load an image
-#frame = cv2.imread('images/PositionAmpel.png')
+#frame = cv2.imread('images/ampel_red.png')
+#frame = cv2.imread('images/ampel_green.png')
+#frame = cv2.imread('images/color_gradient.png')
+
+# Load a video
+#camera = cv2.VideoCapture('videos/AmpelTest.mp4')
 
 # initialize variables for traffic light detection
 # loop: initial state TRUE to run the loop and FALSE to stop the loop
@@ -20,16 +25,16 @@ camera = cv2.VideoCapture(0)
 # emergency_counter: a counter to prevent the pink panzer form never starting
 # even if no signal from the traffic light can be detected
 loop = True
-traffic_light = 'ps'
+traffic_light = 's'
 emergency_counter = 0
 
 # looping while video capture is active
 if (camera.isOpened()):
     while (loop):
 
-        # if the counter reaches 500, the go siganl will be given
+        # if the counter reaches 200, the go siganl will be given
         # regardless of the traffic light
-        if (emergency_counter < 500):
+        if (emergency_counter < 200):
 
             print (traffic_light)
 
@@ -41,7 +46,7 @@ if (camera.isOpened()):
 
             # set the color for color detection and create
             # a mask by checking for red and green traffic lights
-            mask = color_detection(prepped_frame, 'red')
+            mask = color_detection(prepped_frame, 'green')
 
             # find contours in the mask and initialize the current
             # (x, y) center of the ball
@@ -51,7 +56,7 @@ if (camera.isOpened()):
             traffic_light = go_time(contour)
 
             # stop the loop if it is go time
-            if (traffic_light == 'pg'):
+            if (traffic_light == 'g'):
                 print (traffic_light)
                 print ('Green Light detected')
                 loop = False
@@ -59,7 +64,7 @@ if (camera.isOpened()):
             # increase the emergency counter
             emergency_counter += 1
 
-            cv2.imshow('frame', frame)
+            #cv2.imshow('frame', frame)
             cv2.imshow('prepped_frame', prepped_frame)
             cv2.imshow('mask', mask)
             if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -68,7 +73,7 @@ if (camera.isOpened()):
         else:
             # stop the loop if the emergency counter reaches 100
             # and set the traffic light variable accordingly
-            traffic_light = 'pg'
+            traffic_light = 'g'
             loop = False
             print (traffic_light)
             print ('Emergency Loop exeeded')
--- TrafficLightDetection.py
+++ TrafficLightDetection.py
@@ -7,41 +7,47 @@ import cv2
 def frame_prep(frame):
         frame = imutils.resize(frame, width = 600)
         frame = frame[175:365, 475:515]
-        #frame = cv2.GaussianBlur(frame, (11, 11), 0)
-        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        frame = cv2.GaussianBlur(frame, (11, 11), 0)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         return frame
 
 # detects the given color and creates a bit mask for said color
 def color_detection(frame, color):
 
         if (color == 'red'):
-                top_min = np.array([170, 50, 50])
+                top_min = np.array([170, 100, 100])
                 top_max = np.array([180, 255, 255])
-                bottom_min = np.array([  0, 50, 50])
+                bottom_min = np.array([  0, 100, 100])
                 bottom_max = np.array([ 10, 255, 255])
 
+                # construct a mask for the upper bound of the color "red",
+                # then perform a series of dilations and erosions to
+                # remove any small blobs left in the mask
+                mask_top = cv2.inRange(frame, top_min, top_max)
+                mask_top = cv2.erode(mask_top, None, iterations = 5)
+                mask_top = cv2.dilate(mask_top, None, iterations = 5)
+
+                # construct a mask for the bottom bound of the color "red",
+                # then perform a series of dilations and erosions to
+                # remove any small blobs left in the mask
+                mask_bottom = cv2.inRange(frame, bottom_min, bottom_max)
+                mask_bottom = cv2.erode(mask_bottom, None, iterations = 2)
+                mask_bottom = cv2.dilate(mask_bottom, None, iterations = 2)
+
+                # combine the top and bottom masks to create the complete mask
+                mask = cv2.add(mask_top, mask_bottom)
+
         elif (color == 'green'):
-                top = np.array([170, 100, 100])
-                bottom = np.array([170, 100, 100])
+                bottom = np.array([ 30, 100, 100])
+                top = np.array([ 90, 255, 255])
 
-        # construct a mask for the given color,
-        # then perform a series of dilations and erosions to
-        # remove any small blobs left in the mask
-        #mask_top = cv2.inRange(frame, lower_red_top, upper_red_top)
-        mask_top = cv2.inRange(frame, top_min, top_max)
-        mask_top = cv2.erode(mask_top, None, iterations = 2)
-        mask_top = cv2.dilate(mask_top, None, iterations = 2)
-
-        # construct a mask for the bottom bound of the HSV color "red",
-        # then perform a series of dilations and erosions to
-        # remove any small blobs left in the mask
-        #mask_bottom = cv2.inRange(hsv, lower_red_bottom, upper_red_bottom)
-        mask_bottom = cv2.inRange(frame, bottom_min, bottom_max)
-        mask_bottom = cv2.erode(mask_bottom, None, iterations = 2)
-        mask_bottom = cv2.dilate(mask_bottom, None, iterations = 2)
-
-        # combine the top and bottom masks to create the complete mask
-        mask = cv2.add(mask_top, mask_bottom)
+                # construct a mask for the upper bound of the color "green",
+                # then perform a series of dilations and erosions to
+                # remove any small blobs left in the mask
+                mask = cv2.inRange(frame, bottom, top)
+                mask = cv2.erode(mask, None, iterations = 10)
+                mask = cv2.dilate(mask, None, iterations = 10)
+                
         return mask
 
 # returns the found contours in the given mask
@@ -63,9 +69,10 @@ def go_time(contour):
                 center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
                 # only proceed if the radius meets a minimum and maximum size
-                if (radius > 10 and radius < 15):
-                        return 'pg'
+                #if (radius > 10):
+                if (radius > 10):
+                        return 'g'
                 else:
-                        return 'ps'
+                        return 's'
         else:
-                return 'ps'
+                return 's'
