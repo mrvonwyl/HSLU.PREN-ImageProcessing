3b177fdd60a7417a57b76178aea42f992da0d2ac
loderlu <web@a-discretion.com>
2017-06-30 14:03:24 +0200

    AmpelMain ohne Emergency aktiviert

--- AmpelMain.py
+++ AmpelMain.py
@@ -37,57 +37,37 @@ class MainAmpelerkennung:
         if (camera.isOpened()):
             while (loop):
 
-                # if the counter reaches 200, the go siganl will be given
-                # regardless of the traffic light
-                if (emergency_counter < 200):
+                print (traffic_light)
 
+                # grab the current frame
+                _, frame = camera.read()
+
+                # prepare the frame for color detection
+                prepped_frame = frame_prep(frame)
+
+                # set the color for color detection and create
+                # a mask by checking for red and green traffic lights
+                mask = color_detection(prepped_frame, 'green')
+
+                # find contours in the mask and initialize the current
+                # (x, y) center of the ball
+                contour = contour_detection(mask)
+
+                # give the go-signal when a matching contour was found
+                traffic_light = go_time(contour)
+
+                # stop the loop if it is go time
+                if (traffic_light == 'g'):
                     print (traffic_light)
-
-                    # grab the current frame
-                    _, frame = camera.read()
-
-                    # prepare the frame for color detection
-                    prepped_frame = frame_prep(frame)
-
-                    # set the color for color detection and create
-                    # a mask by checking for red and green traffic lights
-                    mask = color_detection(prepped_frame, 'green')
-
-                    # find contours in the mask and initialize the current
-                    # (x, y) center of the ball
-                    contour = contour_detection(mask)
-
-                    # give the go-signal when a matching contour was found
-                    traffic_light = go_time(contour)
-
-                    # stop the loop if it is go time
-                    if (traffic_light == 'g'):
-                        print (traffic_light)
-                        print ('Green Light detected')
-                        loop = False
-
-                    # increase the emergency counter
-                    emergency_counter += 1
-
-                    cv2.imshow('frame', frame)
-                    cv2.imshow('prepped_frame', prepped_frame)
-                    cv2.imshow('mask', mask)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-
-                else:
-                    # stop the loop if the emergency counter reaches 100
-                    # and set the traffic light variable accordingly
-                    traffic_light = 'g'
+                    print ('Green Light detected')
                     loop = False
-                    print (traffic_light)
-                    print ('Emergency Loop exeeded')
 
-                    cv2.imshow('frame', frame)
-                    cv2.imshow('prepped_frame', prepped_frame)
-                    cv2.imshow('mask', mask)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
+                cv2.imshow('frame', frame)
+                cv2.imshow('prepped_frame', prepped_frame)
+                cv2.imshow('mask', mask)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
         else:
             print('No Video Capture detected')
 
